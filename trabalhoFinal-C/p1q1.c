ea0a7cf32640d2a4cc64764a341da959d04b202a
Pietra Paz <81472166+pietrapaz@users.noreply.github.com>
2023-05-01 11:40:30 -0300

    rename cpp to c

--- trabalhoFinal-C/p1q1.c
+++ trabalhoFinal-C/p1q1.c
@@ -1,17 +1,17 @@
-//Engenharia de Computação - 1º semestre matutino - Linguagem de Programação I - Parte 1, q.1
+//Engenharia de ComputaÃ§Ã£o - 1Âº semestre matutino - Linguagem de ProgramaÃ§Ã£o I - Parte 1, q.1
 //Alunos: Ana Carolina, Nicolas, Pietra, Roberto e Sarah
 //5 de dezembro de 2022
 
-/* 1) Crie um programa que carregue (armazene) números numa matriz 3 x 3. Após a leitura dos números, você deverá criar um MENU que ofereça as seguintes opções:
-1 - Mostra a quantidade de números pares armazenados;
-2 - Mostra a soma dos números ímpares armazenados;
-3 - Mostra o maior e o menor número armazenado;
-4 - Mostra a média dos números armazenados;
-5 - Mostra a quantidade de números armazenados que são iguais a 10;
-6 - Mostra a multiplicação dos números armazenados na diagonal principal (índice da linha igual ao índice da coluna);
-7 - Mostra a soma dos números armazenados que são divisíveis por 5;
-8 - Armazena num vetor todos os números menores que 8. Mostrar o vetor resultante;
-9 - Consulta um número na matriz. O usuário deverá informar qual o número deseja procurar na matriz, caso encontre, informar o número e qual a posição (índices) na matriz;
+/* 1) Crie um programa que carregue (armazene) nÃºmeros numa matriz 3 x 3. ApÃ³s a leitura dos nÃºmeros, vocÃª deverÃ¡ criar um MENU que ofereÃ§a as seguintes opÃ§Ãµes:
+1 - Mostra a quantidade de nÃºmeros pares armazenados;
+2 - Mostra a soma dos nÃºmeros Ã­mpares armazenados;
+3 - Mostra o maior e o menor nÃºmero armazenado;
+4 - Mostra a mÃ©dia dos nÃºmeros armazenados;
+5 - Mostra a quantidade de nÃºmeros armazenados que sÃ£o iguais a 10;
+6 - Mostra a multiplicaÃ§Ã£o dos nÃºmeros armazenados na diagonal principal (Ã­ndice da linha igual ao Ã­ndice da coluna);
+7 - Mostra a soma dos nÃºmeros armazenados que sÃ£o divisÃ­veis por 5;
+8 - Armazena num vetor todos os nÃºmeros menores que 8. Mostrar o vetor resultante;
+9 - Consulta um nÃºmero na matriz. O usuÃ¡rio deverÃ¡ informar qual o nÃºmero deseja procurar na matriz, caso encontre, informar o nÃºmero e qual a posiÃ§Ã£o (Ã­ndices) na matriz;
 10 - SAIR*/
 
 #include<stdio.h>
@@ -123,21 +123,21 @@ int main(void) {
 					printf("Valores menores que 8 = %d\n", vet[i]);
 				}
 				if(indice==0){
-					printf("Não há número menor que 8\n");
+					printf("NÃ£o hÃ¡ nÃºmero menor que 8\n");
 				}
 			break;
 			
 			case 9:
-				printf("Qual número da matriz você quer consultar?\n");
+				printf("Qual nÃºmero da matriz vocÃª quer consultar?\n");
 				scanf("%d", &num);
 				for(i=0; i<3; i++){
 					for(j=0; j<3; j++){
 						if(matriz[i][j]==num){
-						printf("O número é %d, na linha %d, coluna %d\n", num, i+1, j+1);	
+						printf("O nÃºmero Ã© %d, na linha %d, coluna %d\n", num, i+1, j+1);	
 						}
 					}
 				}
-			    default: printf("\nNão tem na matriz\n");
+			    default: printf("\nNÃ£o tem na matriz\n");
 			break;
 			
 			case 10:
@@ -146,7 +146,7 @@ int main(void) {
 		}
 	} while (menu!=10);
 	if (menu == 10){
-		printf("\n Muito obrigada! Até mais <3");
+		printf("\n Muito obrigada! AtÃ© mais <3");
 			
 	return 10;}	
 }
